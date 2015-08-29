05fb62dbc9c980a974b251b6579aba5359f138b0
Billy <williampagee@gmail.com>
2015-08-28 21:33:26 -0400

    Init Commit of base web app.

---
+++ Web/src/app/index.module.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('web', [
+      'ngAnimate',
+      'ngCookies',
+      'ngTouch',
+      'ngSanitize',
+      'ngResource',
+      'ui.router',
+      'ui.bootstrap'
+    ]);
+
+})();
